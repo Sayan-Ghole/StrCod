160e71bc30f61fb35ca549406b56d63f32d4d565
Sayan-Ghole <sayanghole126@gmail.com>
2026-02-10 20:43:34 +0530

    new

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,24 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\userController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ErrorExplainer;
 
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/users', [userController::class,'create'])->name('CreateUser');
-Route::post('/users', [userController::class,'store']);
+Route::get('/users', [UserController::class,'create'])->name('CreateUser');
+Route::post('/users', [UserController::class,'store']);
 
-Route::get('/userdetails', [userController::class,'show'])->name('UserDetails');
-Route::get('/update/{id}',[userController::class,'edit'])->name('updateUser');
-Route::post('/update/{id}',[userController::class,'update'])->name('updateUser');
-Route::get('/deleteUser/{id}',[userController::class,'destroy'])->name('deleteUser');
+Route::get('/userdetails', [UserController::class,'show'])->name('UserDetails');
+Route::get('/update/{id}',[UserController::class,'edit'])->name('updateUser');
+Route::post('/update/{id}',[UserController::class,'update'])->name('updateUser');
+Route::get('/deleteUser/{id}',[UserController::class,'destroy'])->name('deleteUser');
 
-Route::get('/login', [userController::class,'loginShow'])->name('loginShow');
-Route::post('/login', [userController::class,'login'])->name('login');
-Route ::get('/logout',[userController::class,'logout'])->name('logout');
+Route::get('/login', [UserController::class,'loginShow'])->name('loginShow');
+Route::post('/login', [UserController::class,'login'])->name('login');
+Route ::get('/logout',[UserController::class,'logout'])->name('logout');
 
 Route::get('/errorExplainer', [ErrorExplainer::class,'index'])->name('errorExplainer');
 Route::post('/errorExplainer', [ErrorExplainer::class,'errorFinder'])->middleware('auth');
